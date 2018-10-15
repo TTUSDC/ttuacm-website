7403124103c0b584342ba79542c2c19b774fefb7
ynigoreyes <ynigoreyes@gmail.com>
2018-10-15 14:22:52 -0500

    adding a doc generator for Node 8

--- .esdoc.js
+++ .esdoc.js
@@ -1,7 +1,22 @@
 module.exports = {
-  "source": "./src",
+  "source": ".",
   "destination": "./docs",
+  "includes": ["^src"],
+  "unexportIdentifier": true,
+  "undocumentIdentifier": false,
+  "title": "TTUACM Backend",
+  "lint": false,
   "plugins": [
-    {"name": "esdoc-standard-plugin"}
-  ]
+    {"name": "esdoc-node"}
+  ],
+  "experimentalProposal": {
+    "classProperties": true,
+    "objectRestSpread": true,
+    "decorators": true,
+    "doExpressions": true,
+    "functionBind": true,
+    "asyncGenerators": true,
+    "exportExtensions": true,
+    "dynamicImport": true
+  }
 }
