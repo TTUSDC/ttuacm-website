df86f3aec621074cd3580156d5d70b51cb8b707d
ynigoreyes <ynigoreyes@gmail.com>
2018-10-15 14:39:35 -0500

    adding docs to utils

--- src/utils/db-connect.js
+++ src/utils/db-connect.js
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose')
 
-module.exports.connectDB = (connection) => {
+const connectDB = (connection) => {
   mongoose.connect(
     connection,
     {
       useNewUrlParser: true,
     },
-  );
+  )
 
   mongoose.connection.on('connect', () => {
     console.log('Connected to MongoDB')
   })
   mongoose.connection.on('error', (err) => {
-    console.error(`Error Connecting to database... \n${err}`);
+    console.error(`Error Connecting to database... \n${err}`)
     console.error(`Tried connecting to ${process.env.db}`)
     process.exit(1);
-  });
-};
+  })
+}
+
+module.exports = connectDB
