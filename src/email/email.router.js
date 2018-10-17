5e20b2a6d5bcff2ddb95bd61c01bef616899cb7f
ynigoreyes <ynigoreyes@gmail.com>
2018-10-17 01:44:44 -0500

    adding documentation to email router and controller

--- src/email/email.router.js
+++ src/email/email.router.js
@@ -1,18 +1,26 @@
-const express = require('express');
+const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
 
 // Controller
-const controller = require('./email.controller');
+const controller = require('./email.controller')
 
-router.get('/test', (req, res) => {
-  res.send('Email App Works!');
-});
+/**
+ * Testing route for the Events Service
+ *
+ * - Endpoint: `/email/api/v2`
+ * - Verb: GET
+ *
+ * @typedef {function} UserRouter
+ */
+router.get('/', (req, res) => {
+  res.send('Email App Works!')
+})
 
 /**
  * Sends and question to ACM Email
  *
- * - Endpoint: `/users/contact-us`
+ * - Endpoint: `/email/api/v2/contact-us`
  * - Verb: POST
  *
  * @typedef {function} UserRouter-contactUs
@@ -23,28 +31,57 @@ router.post('/contact-us', (req, res) => {
     email: req.body.email,
     topic: req.body.topic,
     message: req.body.message,
-  };
+  }
   controller.contactUs(emailInfo)
     .then(() => res.status(200).json())
     .catch(err => {
       console.error(err)
       res.status(404).json()
-    });
-});
+    })
+})
 
+/**
+ * Sends a confirmation email to the user with a link/endpoint
+ * to verify their email. Users will click the link to verify
+ * thier emails and should be redirected to the login page
+ *
+ * - Endpoint: `/email/api/v2/confirm-email`
+ * - Verb: POST
+ *
+ * @typedef {function} UserRouter-sendConfirmationEmail
+ */
 router.post('/confirm-email', (res, req) => {
   console.log(req.body)
-  res.send('confirm-email');
-});
+  res.send('confirm-email')
+})
 
+/**
+ * Sends a reset password email to the user
+ * The user should be able to click on the
+ * link sent and redirected to the reset
+ * password page
+ *
+ * - Endpoint: `/email/api/v2/reset-password`
+ * - Verb: POST
+ *
+ * @typedef {function} UserRouter-sendResetEmail
+ */
 router.post('/reset-email', (res, req) => {
   console.log(req.body)
-  res.send('reset-email');
-});
+  res.send('reset-email')
+})
 
-router.post('/change-password', (res, req) => {
+/**
+ * Notifies the user that their password has been changed
+ *
+ * - Endpoint: `/email/api/v2/change-password-notif`
+ * - Verb: POST
+ *
+ * @typedef {function} UserRouter-sendChangedPasswordNotification
+ */
+router.post('/change-password-notif', (res, req) => {
   console.log(req.body)
-  res.send('change-password');
-});
+  res.send('change-password-notif')
+})
 
-module.exports = router;
+module.exports = router
