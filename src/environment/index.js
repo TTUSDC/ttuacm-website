c38ca8545206691d416c6b75a0cf8cb5342eb14e
ynigoreyes <ynigoreyes@gmail.com>
2018-10-25 00:54:44 -0500

    have to use password to be able to fetch the environment

--- src/environment/index.js
+++ src/environment/index.js
@@ -5,8 +5,14 @@ const cors = require('cors')({ origin: true })
 const app = express()
 app.use(cors)
 
-app.get('/', (req, res) => {
-  res.json(functions.config().environment)
+app.get('/:password', (req, res) => {
+  const { password } = req.params
+  const { secret, environment } = functions.config()
+  if (password === secret.password) {
+    res.json(environment)
+  } else {
+    res.status(401).end()
+  }
 })
 
 module.exports = app
