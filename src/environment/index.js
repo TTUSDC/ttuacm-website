de2f4373677dbca69a8a83ebbeb3ced0f0c860d1
ynigoreyes <ynigoreyes@gmail.com>
2018-10-26 03:04:40 -0500

    protecting route in environement

--- src/environment/index.js
+++ src/environment/index.js
@@ -2,16 +2,32 @@ const express = require('express')
 const functions = require('firebase-functions')
 const cors = require('cors')({ origin: true })
 
+const PROTECTED_ENDPOINTS = [
+  'https://acm-texas-tech-web-app-2-beta.firebaseapp.com/',
+  'https://acm-texas-tech-web-app-2.firebaseapp.com'
+]
+
 const app = express()
 app.use(cors)
 
-app.get('/:password', (req, res) => {
-  const { password } = req.params
-  const { secret, environment } = functions.config()
-  if (password === secret.password) {
-    res.json(environment)
+/**
+ * A Protected route to serve environment variables
+ *
+ * When the route is called in a production/staging environment,
+ * we have to check whether or not the host is either the
+ * production or staging host. Otherwise, send over the environment
+ */
+app.get('/', (req, res) => {
+  const { environment } = functions.config()
+  if (['prod', 'staging'].includes(environment.env)) {
+    if (PROTECTED_ENDPOINTS.includes(req.headers.origin)) {
+      res.json(environment)
+    } else {
+      console.error(`${req.headers.origin} does not match https://acm-texas-tech-web-app-2.firebaseapp.com`)
+      res.status(401).end()
+    }
   } else {
-    res.status(401).end()
+    res.json(environment)
   }
 })
 
