cb24309f9f172659cec3301d072842fede9d1ed2
ynigoreyes <ynigoreyes@gmail.com>
2018-11-26 12:29:44 -0600

    fixed format of events test

--- src/events/events.controller.spec.js
+++ src/events/events.controller.spec.js
@@ -4,7 +4,7 @@ const Controller = require('./events.controller')
 const { expect } = chai
 
 describe('Events Unit Test', () => {
-  it('should be able to fetch events from ACM', (done) => {
+  it('[getAllEvents] should be able to fetch events from ACM', (done) => {
     Controller.getAllEvents((err, events) => {
       expect(err).to.equal(null)
       expect(events.length > 0).to.equal(true)
