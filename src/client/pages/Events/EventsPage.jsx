f7b9a383fc150c3476c5da9b8eca0b8ac4016712
Ynigo Reyes <26727346+ynigoreyes@users.noreply.github.com>
2018-12-24 02:41:46 -0600

    Update src/client/pages/Events/EventsPage.jsx
    Co-Authored-By: madewithsmiles <41027427+madewithsmiles@users.noreply.github.com>

--- src/client/pages/Events/EventsPage.jsx
+++ src/client/pages/Events/EventsPage.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
-import PageInfo from '../../components/PageInfo'
+ import PageInfo from 'components/PageInfo'
 import EventsContainer from '../../containers/EventsContainer'
 
 import { EventsPageInfo } from '../../containers/EventsFakeAPI'
