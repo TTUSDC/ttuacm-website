ba9798dbd61dcceb4346c11d87d6a20e39a3992a
ynigoreyes <ynigoreyes@gmail.com>
2018-12-27 22:53:54 -0600

    moved event card components to component file since we will need it in home page

---
+++ src/client/components/EventCard.spec.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable */
+const EventsResponse = {
+  TODAY: [
+    {
+      month: 'July',
+      day: '28',
+      weekday: 'Saturday',
+      name: 'GitHub Workshop',
+      timeloc: '5:00pm - 6:00pm @ IMSE 117',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut',
+    },
+    {
+      month: 'July',
+      day: '28',
+      weekday: 'Saturday',
+      name: 'GitHub Workshop',
+      timeloc: '5:00pm - 6:00pm @ IMSE 117',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut',
+    },
+  ],
+  TOMORROW: [
+    {
+      month: 'July',
+      day: '28',
+      weekday: 'Saturday',
+      name: 'GitHub Workshop',
+      timeloc: '5:00pm - 6:00pm @ IMSE 117',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut',
+    },
+    {
+      month: 'July',
+      day: '28',
+      weekday: 'Saturday',
+      name: 'GitHub Workshop',
+      timeloc: '5:00pm - 6:00pm @ IMSE 117',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut',
+    },
+  ],
+  'THIS WEEK': [
+    {
+      month: 'July',
+      day: '28',
+      weekday: 'Saturday',
+      name: 'GitHub Workshop',
+      timeloc: '5:00pm - 6:00pm @ IMSE 117',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut',
+    },
+    {
+      month: 'July',
+      day: '28',
+      weekday: 'Saturday',
+      name: 'GitHub Workshop',
+      timeloc: '5:00pm - 6:00pm @ IMSE 117',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut',
+    },
+  ],
+  'THIS MONTH': [],
+}
