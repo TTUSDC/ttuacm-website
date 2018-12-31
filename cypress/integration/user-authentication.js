1bcf31b90b1980e0a48476ca95ee2068ec722c22
ynigoreyes <ynigoreyes@gmail.com>
2018-12-30 21:54:10 -0600

    adding e2e test for registration

--- cypress/integration/user-authentication.js
+++ cypress/integration/user-authentication.js
@@ -3,25 +3,57 @@
 // Please check the host in cypress.json. If runnign locally, change the string to your local development
 
 context('User Authentication', () => {
-  before(async () => {
-    let connection_string = Cypress.env('DB_CONNECTION')
-    if (process.env.CI) connection_string = process.env.DB_CONNECTION
+  const MOCK_USER = {
+    firstName: 'Miggy',
+    lastName: 'Reyes',
+    email: 'miggy.reyes@ttu.edu'
+  }
 
-    console.log(process.env)
-    console.log(mongoose)
+  before(async () => {
+    let connection_string = Cypress.env('API_CONNECTION')
+    if (process.env.CI) connection_string = process.env.API_CONNECTION
 
     try {
-      await mongoose.connect(connection_string, {
-        useNewUrlParser: true,
-      })
-      await mongoose.connection.dropDatabase()
+      await cy.request('DELETE', `${connection_string}/api/v2/auth/seed`, MOCK_USER)
     } catch(err) {
       console.error(err)
       throw err
     }
   })
 
-  it('should be able to register a user', () => {
+  it('should be able to register a user with proper input', () => {
     cy.visit('auth')
+      .get('[data-testid=switch-to-registration]')
+      .click()
+
+    cy
+      .get('[data-testid=FirstName]').type('Miggy')
+      .get('[data-testid=LastName]').type('Reyes')
+      .get('[data-testid=Email]').type('miggy.reyes@ttu.edu')
+      .get('[data-testid=Password]').type('BadPassword')
+      .get('[data-testid=ConfirmPassword]').type('P@ssw0rd')
+      .get('[data-testid=GraduationDate]').type('2022-05-15')
+
+    cy.get('button[disabled]')
+
+  })
+
+  it('should not be able to register a user with bad input', () => {
+    cy.visit('auth')
+      .get('[data-testid=switch-to-registration]')
+      .click()
+
+    cy.get('[data-testid=FirstName]').type('Miggy')
+      .get('button[disabled]')
+      .get('[data-testid=LastName]').type('Reyes')
+      .get('button[disabled]')
+      .get('[data-testid=Email]').type('miggy.reyes@ttu.edu')
+      .get('button[disabled]')
+      .get('[data-testid=Password]').type('BadPassword')
+      .get('button[disabled]')
+      .get('[data-testid=ConfirmPassword]').type('NotMatching')
+      .get('button[disabled]')
+      .get('[data-testid=GraduationDate]').type('2022-05-15')
+      .get('button[disabled]')
   })
 })
