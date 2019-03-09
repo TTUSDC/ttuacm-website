10992b6d54fd2522fda42ac8e4b11858aecc7ed5
ynigoreyes <ynigoreyes@gmail.com>
2019-03-09 16:09:13 -0600

    removed duplicate testing

--- src/api/email/email.spec.js
+++ src/api/email/email.spec.js
@@ -1,22 +1,26 @@
 require('dotenv').config()
 const request = require('supertest')
+let { app } = require('../../../index.js')
 
-let app =
-  'https://us-central1-acm-texas-tech-web-app-2-beta.cloudfunctions.net/api/v2/email'
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 10 // eslint-disable-line
 
-if (!process.env.CI) {
-  app = `${process.env.API_ENDPOINT}/api/v2/email`
+if (process.env.CI) {
+  app = 'https://us-central1-acm-texas-tech-web-app-2-beta.cloudfunctions.net'
+} else {
+  app.listen(8080)
 }
 
-describe('Email Integration Tests', () => {
-  it('should be able to connect to email service', () =>
+describe('Email Unit Tests', () => {
+  const tag = process.env.CI ? '/api' : '' // eslint-disable-line
+
+  it('[test] should be able to connect to email service', () =>
     request(app)
-      .get('/test')
+      .get(`${tag}/v2/email/test`)
       .expect(200))
 
-  it('should be able to send confirmation emails', () =>
+  it('[confirm-email] should be able to send confirmation emails', () =>
     request(app)
-      .post('/confirm-email')
+      .post(`${tag}/v2/email/confirm-email`)
       .send({
         name: 'Some Name',
         email: 'some-email@gmail.com',
@@ -25,9 +29,9 @@ describe('Email Integration Tests', () => {
       })
       .expect(200))
 
-  it('should be able to send reset password emails', () =>
+  it('[reset-password] should be able to send reset password emails', () =>
     request(app)
-      .post('/reset-password')
+      .post(`${tag}/v2/email/reset-password`)
       .send({
         email: 'some-email@gmail.com',
         token: 'test',
--- src/api/events/events.spec.js
+++ src/api/events/events.spec.js
@@ -1,21 +1,38 @@
-const chai = require('chai')
-const Controller = require('./events.controller')
+require('dotenv').config()
+const request = require('supertest')
+const { expect } = require('chai')
+let { app } = require('../../../index.js')
 
-const { expect } = chai
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 // eslint-disable-line
 
-describe('Events Unit Test', () => {
-  let ctrl
+if (process.env.CI) {
+  app = 'https://us-central1-acm-texas-tech-web-app-2-beta.cloudfunctions.net'
+} else {
+  app.listen(8081)
+}
 
-  beforeEach(() => {
-    ctrl = new Controller()
+describe('Email Unit Tests', () => {
+  const tag = process.env.CI ? '/api' : '' // eslint-disable-line
+
+  it('[test] should be able to connect to events service', async () => {
+    try {
+      await request(app)
+        .get(`${tag}/v2/events/test`)
+        .expect(200)
+    } catch (err) {
+      throw err
+    }
   })
 
-  it('[getAllEvents] should be able to fetch events from ACM', async () => {
+  it('[getEvents] should be able to fetch events', async () => {
     try {
-      const events = await ctrl.getAllEvents()
-      expect(events.length > 0).to.equal(true)
+      const { body } = await request(app)
+        .get(`${tag}/v2/events`)
+        .expect(200)
+
+      expect(Array.isArray(body.allEvents)).to.equal(true)
     } catch (err) {
-      expect(err).to.equal(null)
+      throw err
     }
   })
 })
