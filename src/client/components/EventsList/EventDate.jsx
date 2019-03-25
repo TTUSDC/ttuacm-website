6d963708ea4785ff61cacbc4c2f4e6022d9d1c16
ynigoreyes <ynigoreyes@gmail.com>
2019-03-25 18:35:31 -0500

    refectored to use withStyles again

--- src/client/components/EventsList/EventDate.jsx
+++ src/client/components/EventsList/EventDate.jsx
@@ -1,69 +1,60 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
 
-import useWindowSize from 'hooks/useWindowSize'
-import { useTheme } from '@material-ui/styles'
-
-function getStyle(theme, width) {
-  let monthSize = '2rem'
-  let weekSize = '2rem'
-  let daySize = '8rem'
-  let alignment = 'center'
-  let layout = 'column'
-
-  const boldedDay = 'bold'
-
-  if (width < theme.breakpoints.values.sm) {
-    monthSize = '1rem'
-    weekSize = '1rem'
-    daySize = '1rem'
-    alignment = 'space-evenly'
-    layout = 'row'
-  }
+const styles = (theme) => {
+  const SMALL_TEXT_SIZE = '1rem'
 
   const curr = {
     date: {
       display: 'flex',
-      flexDirection: layout,
+      flexDirection: 'column',
       alignItems: 'center',
-      justifyContent: alignment,
+      justifyContent: 'center',
       textAlign: 'center',
       margin: '0px 20px',
+      [theme.breakpoints.down('xs')]: {
+        justifyContent: 'space-evenly',
+        flexDirection: 'row',
+      },
     },
     month: {
       ...theme.typography.h3,
-      fontSize: monthSize,
-      fontWeight: boldedDay,
+      fontSize: '2rem',
+      [theme.breakpoints.down('xs')]: {
+        fontSize: SMALL_TEXT_SIZE,
+      },
+      fontWeight: 'bold',
     },
     weekday: {
       ...theme.typography.h3,
-      fontSize: weekSize,
-      fontWeight: boldedDay,
+      fontSize: '2rem',
+      [theme.breakpoints.down('xs')]: {
+        fontSize: SMALL_TEXT_SIZE,
+      },
+      fontWeight: 'bold',
     },
     day: {
-      fontSize: daySize,
-      fontWeight: boldedDay,
+      fontSize: '4rem',
+      [theme.breakpoints.down('xs')]: {
+        fontSize: SMALL_TEXT_SIZE,
+      },
+      fontWeight: 'bold',
     },
   }
   return curr
 }
 
 const EventDate = ({
-  month, day, weekday,
-}) => {
-  const { width } = useWindowSize()
-  const theme = useTheme()
-  const style = getStyle(theme, width)
-
-  return (
-    <div style={style.date}>
-      <Typography style={style.month}>{month || 'January'}</Typography>
-      <Typography style={style.day}>{day || '1'}</Typography>
-      <Typography style={style.weekday}>{weekday || '1970'}</Typography>
-    </div>
-  )
-}
+  month, day, weekday, classes,
+}) => (
+  <div className={classes.date}>
+    <Typography className={classes.month}>{month || 'January'}</Typography>
+    <Typography className={classes.day}>{day || '1'}</Typography>
+    <Typography className={classes.weekday}>{weekday || '1970'}</Typography>
+  </div>
+)
 
 EventDate.propTypes = {
   // January-Decemeber
@@ -72,6 +63,8 @@ EventDate.propTypes = {
   day: PropTypes.string,
   // Monday-Sunday
   weekday: PropTypes.string,
+  // Styles
+  classes: PropTypes.shape({}),
 };
 
-export default EventDate
+export default withStyles(styles, { withTheme: true })(EventDate)
