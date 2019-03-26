4dd0e83c0649294b5bc97fedf9e0ef74a32b95cf
ynigoreyes <ynigoreyes@gmail.com>
2019-03-26 02:17:23 -0500

    using cp instead of mv

--- scripts/choose_firebase_config.sh
+++ scripts/choose_firebase_config.sh
@@ -1,11 +1,11 @@
 if [ $TRAVIS_BRANCH == 'master' ]
 then
   echo Setting up firebase config for production
-  sudo mv src/client/firebase_config.production.json mv src/client/firebase_config.json
+  sudo cp src/client/firebase_config.production.json src/client/firebase_config.json
 elif [ $TRAVIS_BRANCH == 'next' ]
 then
   echo Setting up firebase config for staging
-  sudo mv src/client/firebase_config.staging.json mv src/client/firebase_config.json
+  sudo cp src/client/firebase_config.staging.json src/client/firebase_config.json
 else
   echo Not setting up firebase
 fi
