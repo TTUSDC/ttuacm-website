ffceadcf7db9f3bb38d7d117db3f21616cb08b04
ynigoreyes <ynigoreyes@gmail.com>
2019-03-26 02:10:54 -0500

    moving sudo to script

--- scripts/choose_firebase_config.sh
+++ scripts/choose_firebase_config.sh
@@ -1,11 +1,11 @@
 if [ $TRAVIS_BRANCH == 'master' ]
 then
   echo Setting up firebase config for production
-  mv src/client/firebase_config.production.json mv src/client/firebase_config.json
+  sudo mv src/client/firebase_config.production.json mv src/client/firebase_config.json
 elif [ $TRAVIS_BRANCH == 'next' ]
 then
   echo Setting up firebase config for staging
-  mv src/client/firebase_config.staging.json mv src/client/firebase_config.json
+  sudo mv src/client/firebase_config.staging.json mv src/client/firebase_config.json
 else
   echo Not setting up firebase
 fi
