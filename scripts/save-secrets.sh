2bde94d46256bd90e4aa2ad824e44d1e248e1103
ynigoreyes <ynigoreyes@gmail.com>
2019-03-26 01:56:20 -0500

    adding firebase configs to travis

--- scripts/save-secrets.sh
+++ scripts/save-secrets.sh
@@ -1,4 +1,5 @@
-tar cvf secrets.tar service_account.json src/client/firebase_config.json src/api/oauth/credentials.json src/api/oauth/token.json
+tar cvf secrets.tar service_account.json src/client/firebase_config.development.json src/client/firebase_config.staging.json src/client/firebase_config.production.json src/api/oauth/credentials.json src/api/oauth/token.json
+
 travis encrypt-file secrets.tar --add
 
 echo "Make sure that you change .travis.yml before commit changes to secrets"
