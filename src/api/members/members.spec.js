8102bd46be675925359ed2fcf448a920c4932806
ynigoreyes <ynigoreyes@gmail.com>
2019-03-27 19:11:56 -0500

    using the development environment

--- src/api/members/members.spec.js
+++ src/api/members/members.spec.js
@@ -4,7 +4,7 @@ const test = require('firebase-functions-test')()
 
 test.mockConfig({
   environment: {
-    env: 'testing',
+    env: 'development',
   },
   connections: {
     protocol: 'protocol',
