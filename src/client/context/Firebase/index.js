55d8e56e2be028e5a8d7fd195c730145caf8f0ea
ynigoreyes <ynigoreyes@gmail.com>
2019-03-31 01:38:28 -0500

    comments

--- src/client/context/Firebase/index.js
+++ src/client/context/Firebase/index.js
@@ -5,6 +5,8 @@ import Firebase from './firebase'
 const withFirebase = React.createContext(null);
 
 function FirebaseProvider({ children = [] }) {
+  // Used because Firebase may not finish initializing
+  // Must listen for the auth state change and change states appropriately
   const [loggedIn, setLoggedIn] = useState(false)
   const firebase = new Firebase()
   firebase.auth.onAuthStateChanged((user) => {
