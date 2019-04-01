2cde6361ab30332bc509432d90d33cede810ee80
ynigoreyes <ynigoreyes@gmail.com>
2019-04-01 01:25:59 -0500

    fixing node env conditional

--- src/client/hooks/useEndpoint.js
+++ src/client/hooks/useEndpoint.js
@@ -50,7 +50,7 @@ export default function useEndpoint(
   { body = {}, method = 'get', headers = {}, params = {}, path = '/' },
   defaultOrDevelopmentValues = [{}],
 ) {
-  if (process.env.NODE_ENV !== 'development')
+  if (process.env.NODE_ENV === 'development')
     return [null, false, defaultOrDevelopmentValues]
   const [state, dispatch] = useReducer(reducer, {
     ...initState,
