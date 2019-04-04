71a36468500c490ce04aaf4e0c560a66ae280360
ynigoreyes <ynigoreyes@gmail.com>
2019-04-04 03:02:38 -0500

    merged Main and SiteContent

--- src/client/Main.jsx
+++ src/client/Main.jsx
@@ -3,20 +3,44 @@ import { PropTypes } from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import NavBar from 'pages/NavBar/NavBar.jsx'
-import SiteContent from './SiteContent.jsx'
+import { connect } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router/immutable'
+import Routes from 'Routes'
+import Footer from 'pages/Footer'
 
-const styles = {
+const styles = (theme) => ({
   Main: {
     display: 'flex',
     flexFlow: 'column wrap',
   },
-}
+  Router: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: '1 0 auto',
+    marginTop: 0,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  SiteContent: {
+    margin: 0,
+    width: '100%',
+  },
+})
 
 const Main = ({ classes = {}, history }) => (
   <div className={classes.Main}>
     <CssBaseline />
     <NavBar />
-    <SiteContent history={history} />
+    <div className={classes.SiteContent}>
+      <main className={classes.Router}>
+        <ConnectedRouter history={history}>
+          <Routes />
+        </ConnectedRouter>
+      </main>
+      <Footer />
+    </div>
   </div>
 )
 
@@ -25,4 +49,4 @@ Main.propTypes = {
   history: PropTypes.shape({}),
 }
 
-export default withStyles(styles)(Main)
+export default connect()(withStyles(styles)(Main))
