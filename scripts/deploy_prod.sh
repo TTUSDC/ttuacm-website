67cd6ac098e055e2112aedce1965210a937f68d6
ynigoreyes <ynigoreyes@gmail.com>
2019-04-12 14:23:51 -0500

    targetting node_modules

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,6 +1,6 @@
 if [[ ${CIRCLE_BRANCH} == "master" ]]; then
   yarn build:client
-  firebase deploy -P acm-texas-tech-web-app-2 --token=$FIREBASE_DEPLOY_TOKEN
+  ./node_modules/.bin/firebase deploy -P acm-texas-tech-web-app-2 --token=$FIREBASE_DEPLOY_TOKEN
 else
   echo Skipping deployment to Production
 fi
--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,6 +1,6 @@
 if [[ ${CIRCLE_BRANCH} == "next" ]]; then
   yarn build:client
-  firebase deploy -P acm-texas-tech-web-app-2-beta --token=$FIREBASE_DEPLOY_TOKEN
+  ./node_modules/.bin/firebase deploy -P acm-texas-tech-web-app-2-beta --token=$FIREBASE_DEPLOY_TOKEN
 else
   echo Skipping deployment to Staging
 fi
