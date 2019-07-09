b7095d8d629b372385da4d571f62330f8f86935c
ynigoreyes <ynigoreyes@gmail.com>
2019-07-09 02:01:51 -0700

    using npm now since yarn isn't working

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,6 +1,6 @@
 if [[ ${CIRCLE_BRANCH} == "master" ]]; then
   mv service_account.production.json service_account.json
-  yarn build:client
+  npm run build:client
   ./node_modules/.bin/firebase deploy -P acm-texas-tech-web-app-2 --token=$FIREBASE_DEPLOY_TOKEN
 else
   echo Skipping deployment to Production
